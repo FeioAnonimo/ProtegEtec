271920397ef8e4c0d0c8a2227e745d4d8600b353
Felipe <83765280+FeioAnonimo@users.noreply.github.com>
2026-03-15 14:28:43 -0300

    arrumar as coisas

--- src/public/index.php
+++ src/public/index.php
@@ -6,11 +6,15 @@
     <title>Document</title>
     <link rel="stylesheet" href="Assets/style.css">
 </head>
-<body>
-    dsadadwasda
+<body>|
+   <container class="container">
+        <form action="login.php" method="POST" class="login-form">
+            <h2>Login</h2>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+    </container>
 </body>
-<script>
-
-    <script src="Assets/script.js"></script>
-</script>
+<script src="Assets/script.js"></script>
 </html>
